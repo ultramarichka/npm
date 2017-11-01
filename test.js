ef6588f748563b4c79743f26ed69307f9b9f43ff
Maryna Sokolova <maryna.sokolova11@gmail.com>
2017-11-01 15:04:16 +0000

    add test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+/*
+     Additional info: If you wanted to actually run any tests you'd written in  
+     `test.js` with the "test" script, you'd run `npm test`. 
+     
+     The docs for npm's scripts property can be found here:  
+     https://docs.npmjs.com/misc/scripts  
+     */
